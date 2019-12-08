f0d1957596e372e51c87b43884cb3ca5a2b00559
Md. Jehadul Karim <53174310+MJKSabit@users.noreply.github.com>
2019-12-09 04:28:01 +0600

    Update iMain.cpp

--- iMain.cpp
+++ iMain.cpp
@@ -1,19 +1,112 @@
 #define _CRT_SECURE_NO_WARNINGS
 # include "iGraphics.h"
 
-int HEIGHT = 400, WIDTH = 600;
+double PI = acos(-1.0);
+int HEIGHT = 500, WIDTH = 450;
 
-double GravitationalForce   = 100;
-double EngineForce          = 30;
-double Mass                 = 10;
+double ANGLE_CHANGE_PER_CLICK = 0.05;
+double MOVEMENT_CHANGE_PER_CLICK = 0.1;
 
-double CenterOfGravity_d, CenterOfGravity_angle;
-double CenterOfGravity_x    = 0.5;
-double CenterOfGravity_y    = 0.4;
+double RAY_MOVEMENT_CHANGE = 0.01;
+
+int m, n;
+char **map/*[10][21] =
+{
+    "####################",
+    "#..................#",
+    "#..................#",
+    "#............##....#",
+    "#.............#....#",
+    "#..................#",
+    "#..................#",
+    "#..................#",
+    "#..................#",
+    "####################"
+}*/;
+
+double fieldOfView = PI/4.0; // 45 degree
+#define numOfRays 100
+double FOV_increament = fieldOfView/(numOfRays-1);
+
+double rayIncreament = 1;
+
+double lineHeight[numOfRays];
+double maxLineHeight = HEIGHT/2;
+
+double playerX=10.0, playerY=5.0;
+double playerAngle=0.0;
+
+void findLineHeight()
+{
+    int i, j;
+    double angle = playerAngle;
+
+    for(i=0; i<numOfRays; i++)
+    {
+        angle = playerAngle + FOV_increament*i;
+
+        double angleSIN = RAY_MOVEMENT_CHANGE*sin(angle);
+        double angleCOS = RAY_MOVEMENT_CHANGE*cos(angle);
+
+        double rayX = playerX, rayY = playerY;
+
+        for(j=0; ; j++){
+
+            if(map[int(rayY)][int(rayX)]!='.'){
+                break;
+            }
+
+            rayX += angleCOS;
+            rayY += angleSIN;
+        }
+
+        lineHeight[i] = maxLineHeight/(j*RAY_MOVEMENT_CHANGE);
+    }
+}
+
+void drawRays()
+{
+    int i;
+    for(i=0; i<numOfRays; i++){
+        /// Intense Coloring
+        int temp = 1000*lineHeight[i]/maxLineHeight;
+        int col = 32 + (temp < 220 ? temp : 220);
+        iSetColor(col, col, col);
+
+        /// Height Drawing
+        iLine(i*WIDTH/(numOfRays-1), HEIGHT/2, i*WIDTH/(numOfRays-1), HEIGHT/2+lineHeight[i]);
+        iLine(i*WIDTH/(numOfRays-1), HEIGHT/2, i*WIDTH/(numOfRays-1), HEIGHT/2-lineHeight[i]);
+    }
+}
+
+void drawMap()
+{
+    iSetColor(10, 250, 10);
+    iFilledRectangle(1, 1, n*10, m*10);
+
+    iSetColor(250, 0, 0);
+    int i, j;
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            if(map[i][j]=='#') iPoint(j*10, i*10, 2);
+        }
+    }
+
+    iSetColor(250, 250, 0);
+    iPoint(playerX*10, playerY*10, 2);
+}
 
 void iDraw()
 {
     iClear();
+    iSetColor(255, 255, 255);
+    //iLine(0, HEIGHT/2, WIDTH, HEIGHT/2);
+
+    findLineHeight();
+    drawRays();
+    drawMap();
 
 }
 
@@ -77,19 +170,31 @@ void iSpecialKeyboard(unsigned char key)
     }
     if(key==GLUT_KEY_RIGHT)
     {
-
+        playerAngle+=ANGLE_CHANGE_PER_CLICK;
     }
     if(key==GLUT_KEY_LEFT)
     {
-
+        playerAngle-=ANGLE_CHANGE_PER_CLICK;
     }
     if(key==GLUT_KEY_UP)
     {
+        playerX += MOVEMENT_CHANGE_PER_CLICK*cos(playerAngle+fieldOfView/2);
+        playerY += MOVEMENT_CHANGE_PER_CLICK*sin(playerAngle+fieldOfView/2);
 
+        if(map[int(playerY)][(int)playerX]!='.'){
+            playerX -= MOVEMENT_CHANGE_PER_CLICK*cos(playerAngle+fieldOfView/2);
+            playerY -= MOVEMENT_CHANGE_PER_CLICK*sin(playerAngle+fieldOfView/2);
+        }
     }
     if(key==GLUT_KEY_DOWN)
     {
+        playerX -= MOVEMENT_CHANGE_PER_CLICK*cos(playerAngle+fieldOfView/2);
+        playerY -= MOVEMENT_CHANGE_PER_CLICK*sin(playerAngle+fieldOfView/2);
 
+        if(map[int(playerY)][(int)playerX]!='.'){
+            playerX += MOVEMENT_CHANGE_PER_CLICK*cos(playerAngle+fieldOfView/2);
+            playerY += MOVEMENT_CHANGE_PER_CLICK*sin(playerAngle+fieldOfView/2);
+        }
     }
     //place your codes for other keys here
 }
@@ -98,9 +203,23 @@ void iSpecialKeyboard(unsigned char key)
 int main()
 {
     //place your own initialization codes here.
-    //freopen("map.txt","r",stdin);
+    freopen("map.txt","r",stdin);
 
+    int i;
+    scanf("%d %d", &m, &n);
 
-    iInitialize(WIDTH, HEIGHT, "Gravity Defied");
+    map = (char **) malloc(m*sizeof(char *));
+    for(i=0; i<m; i++){
+        map[i] = (char*) malloc(n+2);
+        scanf("%s", map[i]);
+    }
+
+    //printf("Player position: ");
+    scanf("%lf %lf", &playerX, &playerY);
+
+    //playerX = n - playerX;
+    //playerY = m - playerY;
+
+    iInitialize(WIDTH, HEIGHT, "3D FPS World by Sabit");
     return 0;
 }
